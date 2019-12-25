e8f83753072472a43ffaf66fc12cdbcbb20cd72b
Dilip Kr. Shukla <contact@fusionstrings.com>
2019-12-25 16:03:08 +0100

    puppeteer fixes

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
-const puppeteer = require('puppeteer');
+module.exports = async (request, response) => {
+    try {
+        const { url = 'https://chhap.glitch.me' } = request.query;
 
-module.exports = async (request, response)=> {
-  try {
-    const {url = 'https://chhap.glitch.me'} = request.query;
-    
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    response.type('png').send(await page.screenshot());
-    await browser.close();
-  } catch (error) {
-    response.status(503).end(error.message);
-  }
-}
+        const browser = await puppeteer.launch({
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless
+        });
+        const page = await browser.newPage();
+        await page.goto(url);
+        response.type('png').send(await page.screenshot());
+        await browser.close();
+    } catch (error) {
+        response.status(503).end(error.message);
+    }
+};
