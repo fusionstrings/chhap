58eed30e26a5795ad47e2f68187b62a92839fe6c
Dilip Kr. Shukla <contact@fusionstrings.com>
2019-12-25 16:27:57 +0100

    fix2

--- api/index.js
+++ api/index.js
@@ -3,16 +3,21 @@ const puppeteer = require('puppeteer-core');
 
 module.exports = async (request, response) => {
     try {
-        const { url = 'https://chhap.glitch.me' } = request.query;
+        const { url = 'https://chhap.glitch.me', type="png" } = request.query;
 
+        const urlWithProtocol = url.startsWith('http') ? url : 'https://' + url;
         const browser = await puppeteer.launch({
             args: chrome.args,
             executablePath: await chrome.executablePath,
             headless: chrome.headless
         });
+
         const page = await browser.newPage();
-        await page.goto(url);
-        response.type('png').send(await page.screenshot());
+        await page.goto(urlWithProtocol);
+        response.statusCode = 200;
+
+        response.setHeader('Content-Type', `image/${type}`);
+        response.send(await page.screenshot({ type }));
         await browser.close();
     } catch (error) {
         response.status(503).end(error.message);
