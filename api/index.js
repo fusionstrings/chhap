51f1707ff25635b7d68014009f11e15c3a587033
Dilip Kr. Shukla <contact@fusionstrings.com>
2020-02-05 23:02:44 +0100

    base64

--- api/index.js
+++ api/index.js
@@ -2,70 +2,79 @@ const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 module.exports = async (request, response) => {
-    try {
-        const {
-            url = 'https://chhap.glitch.me',
-            type = 'png',
-            size,
-            device = 'iPhone XR',
-            encoding = 'binary'
-        } = request.query;
+	try {
+		const {
+			url = 'https://chhap.glitch.me',
+			type = 'png',
+			size,
+			device = 'iPhone XR',
+			encoding = 'binary'
+		} = request.query;
 
-        const urlWithProtocol = url.startsWith('http') ? url : 'https://' + url;
-        const browser = await puppeteer.launch({
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless
-        });
+		const urlWithProtocol = url.startsWith('http') ? url : 'https://' + url;
+		const browser = await puppeteer.launch({
+			args: chrome.args,
+			executablePath: await chrome.executablePath,
+			headless: chrome.headless
+		});
 
-        const deviceProfile = puppeteer.devices[device];
+		const deviceProfile = puppeteer.devices[device];
 
-        const page = await browser.newPage();
+		const page = await browser.newPage();
 
-        if (deviceProfile) {
-            await page.emulate(deviceProfile);
-        }
+		if (deviceProfile) {
+			await page.emulate(deviceProfile);
+		}
 
-        const viewport = {
-            ...page.viewport()
-        };
+		const viewport = {
+			...page.viewport()
+		};
 
-        let fullPage = true;
-    
-        if (size) {
-          const [width, height] = size.split(",").map(item => Number(item));
-          if (!(isFinite(width) && isFinite(height))) {
-            return response
-              .status(400)
-              .send("Malformed size parameter. Example: ?size=800,600");
-          }
-          viewport.width = width;
-          viewport.height = height;
-    
-          fullPage = false;
-        }
-    
-        await page.setViewport(viewport);
-        await page.goto(urlWithProtocol, { waitUntil: "networkidle0" });
-    
-        const options = {
-            fullPage: false,
-            type,
-            encoding
-        };
-        options.clip = {
-          x: 0,
-          y: 0,
-          width: viewport.width,
-          height: viewport.height
-        };
+		let fullPage = true;
 
-        response.statusCode = 200;
-        response.setHeader('Content-Type', `image/${type}`);
-        response.send(await page.screenshot(options));
+		if (size) {
+			const [width, height] = size.split(',').map(item => Number(item));
+			if (!(isFinite(width) && isFinite(height))) {
+				return response
+					.status(400)
+					.send('Malformed size parameter. Example: ?size=800,600');
+			}
+			viewport.width = width;
+			viewport.height = height;
 
-        await browser.close();
-    } catch (error) {
-        response.status(503).end(error.message);
-    }
+			fullPage = false;
+		}
+
+		await page.setViewport(viewport);
+		await page.goto(urlWithProtocol, { waitUntil: 'networkidle0' });
+
+		const options = {
+			fullPage: false,
+			type,
+			encoding
+		};
+		options.clip = {
+			x: 0,
+			y: 0,
+			width: viewport.width,
+			height: viewport.height
+		};
+
+		response.statusCode = 200;
+		response.setHeader('Content-Type', `image/${type}`);
+		const screenshot = await page.screenshot(options);
+		if (encoding === 'base64') {
+			const data = Buffer.from(screenshot, 'base64');
+
+			response.setHeader('Content-Length', data.length);
+
+			response.send(data);
+		} else {
+			response.send(screenshot);
+		}
+
+		await browser.close();
+	} catch (error) {
+		response.status(503).end(error.message);
+	}
 };
